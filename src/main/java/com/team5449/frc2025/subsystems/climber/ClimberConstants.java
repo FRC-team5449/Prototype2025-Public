934792d9c08297f42fc97b0c97975ae0ddbb5b5e
tomtan <tomtan20060613@outlook.com>
2025-03-03 18:15:27 +0800

    Climber Update

---
+++ src/main/java/com/team5449/frc2025/subsystems/climber/ClimberConstants.java
@@ -0,0 +1,14 @@
+// Copyright (c) 2025 FRC 5449
+// http://github.com/frc-team5449
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
+package com.team5449.frc2025.subsystems.climber;
+
+import com.team5449.lib.subsystems.ServoMotorSubsystemConfig;
+
+public class ClimberConstants {
+  public static final ServoMotorSubsystemConfig kClimberConfig = new ServoMotorSubsystemConfig();
+}
---
+++ src/main/java/com/team5449/frc2025/subsystems/climber/ClimberSubsystem.java
@@ -0,0 +1,49 @@
+// Copyright (c) 2025 FRC 5449
+// http://github.com/frc-team5449
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
+package com.team5449.frc2025.subsystems.climber;
+
+import static edu.wpi.first.units.Units.Rotation;
+
+import com.team5449.lib.subsystems.MotorIO;
+import com.team5449.lib.subsystems.MotorInputsAutoLogged;
+import com.team5449.lib.subsystems.ServoMotorSubsystem;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import java.util.function.Supplier;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.littletonrobotics.junction.AutoLogOutput;
+
+public class ClimberSubsystem extends ServoMotorSubsystem<MotorInputsAutoLogged, MotorIO> {
+  @AutoLogOutput @Setter @Getter private ClimberState desiredState = ClimberState.IDLE;
+
+  public ClimberSubsystem(final MotorIO io) {
+    super(ClimberConstants.kClimberConfig, new MotorInputsAutoLogged(), io);
+    setCurrentPositionAsZero();
+    setDefaultCommand(motionMagicSetpointCommand(this::getStateAngle));
+  }
+
+  public Angle getStateAngle() {
+    return desiredState.goalSetpoint.get();
+  }
+
+  public Command setState(ClimberState state) {
+    return Commands.runOnce(() -> setDesiredState(state));
+  }
+
+  @RequiredArgsConstructor
+  public enum ClimberState {
+    IDLE(() -> Rotation.of(0)),
+    ALIGN(() -> Rotation.of(0)),
+    CLIMB(() -> Rotation.of(0));
+
+    public final Supplier<Angle> goalSetpoint;
+  }
+}
